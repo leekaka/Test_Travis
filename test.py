e08d6cbfa8413e1b586a5c9f5f6a8b1ce987e5d6
leekaka <lika27@163.com>
2018-10-28 13:11:21 +0800

    测试一下

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 import os
 from common import *
+import smtplib
+
 
 print(sender)
+
+try:
+    s = smtplib.SMTP(mail_host,"25")
+    s.login(sender,password)
+    s.quit()
+except:
+    print("error")
+
